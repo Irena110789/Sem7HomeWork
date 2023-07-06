2c1fd5c7febfde7a6ea17794b11f00c527dd06c2
Irina <irena-sunny@list.ru>
2023-07-06 22:12:48 +0300

    :initial commit

---
+++ Task50/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] CreateNewArray()
+{
+    int[,] array = new int[new Random().Next(2, 5), new Random().Next(2, 5)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-3, 10);
+        }
+
+    }return array;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]);
+            Console.Write(" ");
+        }Console.WriteLine();
+    }
+}
+
+
+void FoundElements(int [,]array, int row, int column )
+{
+   if( row <= array.GetLength(0) && column <= array.GetLength(1))
+   Console.WriteLine(array[row,column]);
+   else Console.WriteLine("There's no number with such index in the array.");
+}
+
+Console.WriteLine("Input row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input column: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] newArray = CreateNewArray();
+ShowArray(newArray);
+FoundElements(newArray, row, column);
---
+++ Task52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CraateIntRandomArray(int size, int minValue, int maxValue)
+{
+ int[,] array = new int[size, size];
+ for (int i = 0; i < size; i++)
+    {
+     for (int j = 0; j < size; j++)
+        {
+         array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+     for (int j = 0; j < array.GetLength(1); j++)
+        {
+         Console.Write(" " + array[i, j] + "    ");
+        }
+        Console.WriteLine();
+    }
+ Console.WriteLine();
+}
+
+void FoundAverageOfColumns(int[,] array)
+{
+ double[] avarage = new double[array.GetLength(1)];
+ double sum = 0;
+ double size = array.GetLength(1);
+ for (int j = 0; j < array.GetLength(1); j++)
+   {
+     for (int i = 0; i < array.GetLength(0); i++)
+      {
+       sum = sum + array[i, j];
+      }
+     avarage[j] = sum / size;
+     Console.Write(" " + Math.Round(avarage[j], 2) + " ");
+     sum = 0;
+   }
+}
+
+Console.WriteLine(" Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input row index: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input column index: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] newArray = CraateIntRandomArray(size, min, max);
+ShowArray(newArray);
+Console.WriteLine("Avarage of columns is: ");
+FoundAverageOfColumns(newArray);
+
